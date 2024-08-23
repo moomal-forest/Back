9ad1ea82ecfd87987812f70aa08befa59652b387
Jeong Yeonkyung <edaily0129@gmail.com>
2024-08-24 01:55:26 +0900

    이거 pull 꼭 해야 서버가 돌아간다 git add routes/diaryRoutes.js!

--- Back/models/diary.js
+++ Back/models/diary.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
+/*
 const diarySchema = new Schema({
         name: { type: String, required: true},
         color: { type: String, required: true},
@@ -10,8 +10,9 @@ const diarySchema = new Schema({
         members: [{ type: Schema.Types.ObjectId, ref: 'Users' }], //멤버의 userID 배열
         created_at: { type: Date, default: Date.now}
 });
+*/
 
-/* 포스트맨 테스트용 (created_by를 string으로 지정함)
+//포스트맨 테스트용 (created_by를 string으로 지정함)
 const diarySchema = new Schema({
         name: { type: String, required: true},
         color: { type: String, required: true},
@@ -20,7 +21,7 @@ const diarySchema = new Schema({
         members: [{ type: String }], //멤버의 user_id 배열
         created_at: { type: Date, default: Date.now}
 });
-*/
+
 
 const Diary = mongoose.model('diary', diarySchema, 'diaries');
 module.exports = Diary;
